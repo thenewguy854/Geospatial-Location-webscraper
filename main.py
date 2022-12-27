f03857887443a53e39b9bf53a441b3423f17f380
Chuck Husak <thenewguy854@yahoo.com>
2022-12-27 08:16:28 -1000

    Putting main on github

---
+++ main.py
@@ -0,0 +1,48 @@
+import pandas as pd
+from bs4 import BeautifulSoup as BS
+import requests
+import csv
+
+Target_Lat, Target_Log = 37.065984, -79.601172
+
+addresses = pd.read_csv('addresses.csv', delimiter=",")
+list_of_addresses = [x for x in addresses.values]
+
+# Bellow creates all the URLs to check the address in the given CSV
+length = len(list_of_addresses)
+list_of_x_coords = []
+list_of_y_coords = []
+for i in range(10):
+    url = "https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address=" + \
+          list_of_addresses[i][1].replace(" ", "%20").replace("#", "%23").replace("'", "%27") + "%2C" + \
+          list_of_addresses[i][2].replace(" ", "%") + "&benchmark=4"
+
+    page = requests.get(url)
+    html_outlines = BS(page.content, "html.parser")
+
+    coordinate = html_outlines.find_all("span")
+
+    y_coord = coordinate[7:8]
+    x_coord = coordinate[8:9]
+    list_x = [str(i) for i in x_coord]
+    list_y = [str(i) for i in y_coord]
+    for x_c in list_x:
+        list_of_x_coords.append(float(x_c[6:15]))
+        print(list_of_x_coords)
+        to_csv = pd.DataFrame(list_of_x_coords)
+        to_csv.to_csv("Coordinates.csv")
+        list_of_x_coords.clear()
+
+    for y_c in list_y:
+        list_of_y_coords.append(float(y_c[6:16]))
+        # to_csv = pd.DataFrame(list_of_y_coords)
+        # to_csv.to_csv("Coordinates.csv")
+        # list_of_y_coords.clear()
+
+
+# to_csv = pd.DataFrame(list_of_x_coords)
+# to_csv.to_csv("Coordinates.csv", index=False)
+
+
+
+# line 922 in csv is answer 37.068364, -79.681218, 0.00238, 0.080046 Richard Howe,90 Journeys End Dr,Wirtz
