2951e368f112f46601af9633798df6bf02f2d0a4
Chuck Husak <thenewguy854@yahoo.com>
2022-12-29 23:09:11 -1000

    Completed identification of the closest address to target lat and long.

--- main.py
+++ main.py
@@ -1,48 +1,53 @@
 import pandas as pd
 from bs4 import BeautifulSoup as BS
 import requests
-import csv
+from operator import add
 
-Target_Lat, Target_Log = 37.065984, -79.601172
+Target_Lat, Target_Long = 37.065984, -79.601172
 
 addresses = pd.read_csv('addresses.csv', delimiter=",")
 list_of_addresses = [x for x in addresses.values]
 
-# Bellow creates all the URLs to check the address in the given CSV
+# Below creates all the URLs using a given CSV file in (name, address, city) format.
 length = len(list_of_addresses)
 list_of_x_coords = []
 list_of_y_coords = []
-for i in range(length):
+diff_of_lat = []
+diff_of_long = []
+for i in range(len(list_of_addresses)):
     url = "https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address=" + \
           list_of_addresses[i][1].replace(" ", "%20").replace("#", "%23").replace("'", "%27") + "%2C" + \
-          list_of_addresses[i][2].replace(" ", "%") + "&benchmark=4"
+          list_of_addresses[i][2].replace(" ", "%20") + "&benchmark=4"
 
     page = requests.get(url)
     html_outlines = BS(page.content, "html.parser")
-
     coordinate = html_outlines.find_all("span")
-
     y_coord = coordinate[7:8]
     x_coord = coordinate[8:9]
+
+    #checks if data returned from website is valid if not replaces it with 99.99999999
+    if len(x_coord) == 0:
+        x_coord = ["<span>99.99999999999999</span>"]
+        y_coord = ["<span>99.99999999999999</span>"]
     list_x = [str(i) for i in x_coord]
     list_y = [str(i) for i in y_coord]
+
+    #converts all the data from a website string into a float and stores in a list
     for x_c in list_x:
         list_of_x_coords.append(float(x_c[6:15]))
-        # to_csv = pd.DataFrame(list_of_x_coords)
-        # to_csv.to_csv("Coordinates.csv")
+        diff_of_lat.append(Target_Lat - float(x_c[6:15]))
         for y_c in list_y:
             list_of_y_coords.append(float(y_c[6:16]))
+            diff_of_long.append(Target_Long - float(y_c[6:16]))
 
+#calculates the closest lat and long to the target lat and long
+res1 = list(map(abs, diff_of_lat))
+res2 = list(map(abs, diff_of_long))
+res3 = list(map(add, res1, res2))
+index3 = res3.index(min(res3))
+print(list_of_x_coords[index3], list_of_y_coords[index3], list_of_addresses[index3][0])
 
 
 Lat_and_long = {"X": list_of_x_coords, "Y": list_of_y_coords}
-print(Lat_and_long)
 to_csv = pd.DataFrame(Lat_and_long)
 to_csv.to_csv("Coordinates.csv")
-
-
-
-
-
-
-# line 922 in csv is answer 37.068364, -79.681218, 0.00238, 0.080046 Richard Howe,90 Journeys End Dr,Wirtz
